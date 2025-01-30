3025d5392bbdec5954bb8b947fa502294a5ce59b
Antony Reyes Perez <antonyreyes6542@gmail.com>
2025-01-29 22:03:28 -0500

    esto es otro commit

--- EjercicioPrueba2.java
+++ EjercicioPrueba2.java
@@ -1,5 +1,5 @@
 public class EjercicioPrueba2 {
     public static void main(String[] args) {
-        System.out.println("Estoo es un commit2 para git numero 2");
+        System.out.println("Estoo es un commit para git numero 2");
     }
 }
--- EjercicioPrueba3.java
+++ EjercicioPrueba3.java
@@ -1,5 +1,5 @@
 public class EjercicioPrueba3 {
     public static void main(String[] args) {
-        System.out.println("Estoo es un commit2 para git 3");
+        System.out.println("Estoo es un commit para git 3");
     }
 }
