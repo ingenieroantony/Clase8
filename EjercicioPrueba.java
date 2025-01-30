501a3e1c3366b8e0c0685559a12db0a0b2acb2b6
Antony Reyes Perez <antonyreyes6542@gmail.com>
2025-01-29 21:55:09 -0500

    este es mi tercer commit

--- EjercicioPrueba.java
+++ EjercicioPrueba.java
@@ -2,7 +2,7 @@ public class EjercicioPrueba {
 public static void main(String[] args) {
     int a = 8;
     int b = 2;
-    int resultado = a + b;
+    int resultado = a - b;
     System.out.println("El resultado es : " + resultado);
 }
     
--- EjercicioPrueba3.java
+++ EjercicioPrueba3.java
@@ -1,5 +1,5 @@
 public class EjercicioPrueba3 {
     public static void main(String[] args) {
-        System.out.println("Estoo es un commit2 para git hub 3");
+        System.out.println("Estoo es un commit2 para git 3");
     }
 }
